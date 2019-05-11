c5c79768e8f86c69f32afd1268c930454c88c57f
ruochen.shen <ruochen.shen@thoughtworks.com>
2019-05-11 16:12:37 +0800

    first commit

---
+++ FB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(FizzBuzz.solution(i));
+            }
+
+        }
+
+    }
+}
